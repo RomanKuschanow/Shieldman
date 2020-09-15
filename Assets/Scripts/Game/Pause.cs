a5939c1e2e202f0db2dc22a9a41e765f5edb2e59
Your Name <you@example.com>
2020-09-15 19:00:40 +0300

    Рефакторинг паузы

--- Assets/Scripts/Game/Pause.cs
+++ Assets/Scripts/Game/Pause.cs
@@ -6,31 +6,25 @@ public class Pause : MonoBehaviour
 {
     public int Animations = 1;
     public bool AnimationStart;
-    public string AniName;
+    public string AnimationName;
     public GameObject dark, counter, arrow;
-    
-    //запуск анимации
+
     void Update()
     {
         if (AnimationStart == true)
-            GetComponent<Animation>().Play(AniName);
+            GetComponent<Animation>().Play(AnimationName);
     }
 
-    //Условия запуска завершающей анимации
+    //Возобновление игры
     void OnMouseDown()
     {
         if (Animations == 2)
         {
-            AnimationStart = true;
-            AniName = "Pause animation left 2";
-            dark.transform.position = new Vector3(-25, 0, -4.4f);
-            counter.transform.position = new Vector3(0, 12, -3);
-        } else if (Animations == 4)
+            PlayAnimation(2);
+        }
+        else if (Animations == 4)
         {
-            AnimationStart = true;
-            AniName = "Pause animation rigth 2";
-            dark.transform.position = new Vector3(-25, 0, -4.4f);
-            counter.transform.position = new Vector3(0, 12, -3);
+            PlayAnimation(4);
         }
     }
 
@@ -39,17 +33,62 @@ public class Pause : MonoBehaviour
     {
         if (Animations == 1)
         {
-            AnimationStart = true;
-            AniName = "Pause animation left 1";
-            dark.transform.position = new Vector3(0, 0, -4.4f);
-            arrow.transform.position = new Vector3(-20, 19, -1);
-        } else if (Animations == 3)
-        {
-            AnimationStart = true;
-            AniName = "Pause animation rigth 1";
-            dark.transform.position = new Vector3(0, 0, -4.4f);
-            arrow.transform.position = new Vector3(-20, 19, -1);
+            PlayAnimation(1);
         }
+        else if (Animations == 3)
+        {
+            PlayAnimation(3);
+        }
+    }
+
+    public void PlayAnimation(int AnimationId)
+    {
+        Vector3 DartPosition = Vector3.zero;
+        Vector3 ArrowPosition = Vector3.zero;
+        Vector3 CounterPosition = Vector3.zero;
+
+        switch (AnimationId)
+        {
+            case 1:
+                AnimationName = "PauseStartLeft";
+                DartPosition = new Vector3(0, 0, -4.4f);
+                ArrowPosition = new Vector3(-20, 19, -1);
+                break;
+
+            case 2:
+                AnimationName = "PauseEndLeft";
+                DartPosition = new Vector3(-25, 0, -4.4f);
+                CounterPosition = new Vector3(0, 12, -3);
+                break;
+
+            case 3:
+                AnimationName = "PauseStartRigth";
+                DartPosition = new Vector3(0, 0, -4.4f);
+                ArrowPosition = new Vector3(-20, 19, -1);
+                break;
+
+            case 4:
+                AnimationName = "PauseEndRigth";
+                DartPosition = new Vector3(-25, 0, -4.4f);
+                CounterPosition = new Vector3(0, 12, -3);
+                break;
+
+            default:
+                throw new System.ArgumentException(string.Concat("Не найдена анимация с идентификатором", AnimationId));
+        }
+
+        AnimationStart = true;
+
+        dark.transform.position = DartPosition;
+        if (ArrowPosition != Vector3.zero)
+        {
+            arrow.transform.position = ArrowPosition;
+        }
+        else if (CounterPosition != Vector3.zero)
+        {
+            counter.transform.position = CounterPosition;
+        }
+
     }
 
     //Событие при завершении 1, 2 и 3 анимации
