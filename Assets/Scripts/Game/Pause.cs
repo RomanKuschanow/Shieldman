37e6c48fc8699ed4b35dc3bb251967a47786b7ef
Your Name <you@example.com>
2020-09-15 19:29:58 +0300

    Фикс паузы

--- Assets/Scripts/Game/Pause.cs
+++ Assets/Scripts/Game/Pause.cs
@@ -4,82 +4,58 @@ using UnityEngine;
 
 public class Pause : MonoBehaviour
 {
-    public int Animations = 1;
-    public bool AnimationStart;
-    public string AnimationName;
-    public GameObject dark, counter, arrow;
+    [Header("Объекты")]
+    public GameObject dark;
+    public GameObject counter;
+    public GameObject arrow;
 
-    void Update()
+    private Animation Animation;
+
+
+    private void Start()
     {
-        if (AnimationStart == true)
-            GetComponent<Animation>().Play(AnimationName);
+        Animation = GetComponent<Animation>();
     }
+
 
     //Возобновление игры
-    void OnMouseDown()
+    private void OnMouseDown()
     {
-        if (Animations == 2)
-        {
-            PlayAnimation(2);
-        }
-        else if (Animations == 4)
-        {
-            PlayAnimation(4);
-        }
+        PlayAnimation("PauseEndLeft");
     }
+
 
     //Условия запуска начальной анимации
-    public void StartPauseAnimation()
+    private void StartPauseAnimation()
     {
-        if (Animations == 1)
-        {
-            PlayAnimation(1);
-        }
-        else if (Animations == 3)
-        {
-            PlayAnimation(3);
-        }
+        PlayAnimation("PauseStartLeft");
     }
 
-    public void PlayAnimation(int AnimationId)
+
+    private void PlayAnimation(string AnimationName)
     {
         Vector3 DartPosition = Vector3.zero;
         Vector3 ArrowPosition = Vector3.zero;
         Vector3 CounterPosition = Vector3.zero;
 
-        switch (AnimationId)
+        switch (AnimationName)
         {
-            case 1:
-                AnimationName = "PauseStartLeft";
+            case "PauseStartLeft":
                 DartPosition = new Vector3(0, 0, -4.4f);
                 ArrowPosition = new Vector3(-20, 19, -1);
                 break;
 
-            case 2:
-                AnimationName = "PauseEndLeft";
-                DartPosition = new Vector3(-25, 0, -4.4f);
-                CounterPosition = new Vector3(0, 12, -3);
-                break;
-
-            case 3:
-                AnimationName = "PauseStartRigth";
-                DartPosition = new Vector3(0, 0, -4.4f);
-                ArrowPosition = new Vector3(-20, 19, -1);
-                break;
-
-            case 4:
-                AnimationName = "PauseEndRigth";
+            case "PauseEndLeft":
                 DartPosition = new Vector3(-25, 0, -4.4f);
                 CounterPosition = new Vector3(0, 12, -3);
                 break;
 
             default:
-                throw new System.ArgumentException(string.Concat("Не найдена анимация с идентификатором", AnimationId));
+                throw new System.ArgumentException(string.Concat("Не найдена анимация с названием: ", AnimationName));
         }
 
-        AnimationStart = true;
-
         dark.transform.position = DartPosition;
+
         if (ArrowPosition != Vector3.zero)
         {
             arrow.transform.position = ArrowPosition;
@@ -89,19 +65,6 @@ public class Pause : MonoBehaviour
             counter.transform.position = CounterPosition;
         }
 
-    }
-
-    //Событие при завершении 1, 2 и 3 анимации
-    public void EndAnimations1_3()
-    {
-        Animations += 1;
-        AnimationStart = false;
-    }
-
-    //Событие при завершении 4 анимации
-    public void EndAnimations4()
-    {
-        Animations = 1;
-        AnimationStart = false;
+        Animation.Play(AnimationName);
     }
 }
